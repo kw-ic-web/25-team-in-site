f06548973dfb7b933d91e0d6bbdaa086700b1914
Seo Hyuntaek <skht01@naver.com>
2025-10-27 11:30:07 +0900

    Add Login Page

--- src/app/layouts/PublicLayout.tsx
+++ src/app/layouts/PublicLayout.tsx
@@ -1,7 +1,13 @@
 import { Outlet } from "react-router-dom";
+
 export default function PublicLayout() {
   return (
-    <div style={{ minHeight:"100vh", display:"grid", placeItems:"center", background:"#f3f4f6" }}>
+    <div
+      style={{
+        minHeight: "100vh",
+        width: "100%",
+      }}
+    >
       <Outlet />
     </div>
   );
