7d80ad9dfda3cab31a84708a62a59fa7fe1832d6
leehyowon14 <57023119+leehyowon14@users.noreply.github.com>
2025-11-04 01:52:14 +0900

    feat: 이메일 형식 검증 및 일관성 보장
    - regex를 활용한 이메일 형식 검증
    - trim과 lowercase 옵션 추가를 통한 일관성 보장

--- back-end/src/entity/user.js
+++ back-end/src/entity/user.js
@@ -4,7 +4,14 @@ const userSchema = new mongoose.Schema(
   {
     user_id: { type: String, required: true, unique: true },
     user_pw: { type: String, required: true },
-    user_email: { type: String, required: true, unique: true },
+    user_email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, "유효한 이메일 주소를 입력해주세요"],
+    },
     user_xp: { type: Number, default: 0 },
     user_level: { type: Number, default: 1 },
   },
