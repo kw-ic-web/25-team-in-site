8aaa23276204c2e85296f648b6c8d4bf5ad39fdc
leehyowon14 <57023119+leehyowon14@users.noreply.github.com>
2025-11-04 02:34:59 +0900

    fix: db 미연결 상태로 서버가 시작될 수 있던 문제 해결

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -6,26 +6,28 @@ import connectDB from "./config/db.js";
 const port = config.port;
 app.set("port", port);
 
-connectDB();
-const server = http.createServer(app);
+(async () => {
+  await connectDB();
+  const server = http.createServer(app);
 
-server.on("error", (error) => {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-
-  const bind = typeof port === "string" ? `Pipe ${port}` : `Port ${port}`;
-
-  switch (error.code) {
-    case "EACCES":
-      error.message = `${bind} requires elevated privileges`;
+  server.on("error", (error) => {
+    if (error.syscall !== "listen") {
       throw error;
-    case "EADDRINUSE":
-      error.message = `${bind} is already in use`;
-      throw error;
-    default:
-      throw error;
-  }
-});
+    }
 
-server.listen(port);
+    const bind = typeof port === "string" ? `Pipe ${port}` : `Port ${port}`;
+
+    switch (error.code) {
+      case "EACCES":
+        error.message = `${bind} requires elevated privileges`;
+        throw error;
+      case "EADDRINUSE":
+        error.message = `${bind} is already in use`;
+        throw error;
+      default:
+        throw error;
+    }
+  });
+
+  server.listen(port);
+})();
