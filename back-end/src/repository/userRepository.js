7137ed76c691d4cdac205604ed411d7ed72ed1cf
leehyowon14 <57023119+leehyowon14@users.noreply.github.com>
2025-11-04 20:08:10 +0900

    feat: 인스턴스 메서드 활용을 위한 리턴값 변경

--- back-end/src/repository/userRepository.js
+++ back-end/src/repository/userRepository.js
@@ -1,11 +1,11 @@
 import { User } from "../entity/user.js";
 
 export const UserRepository = {
-  async create(id, pw, email) {
-    const user = await User.create(id, pw, email);
-    return user.toObject();
+  async create({ user_id, user_pw, user_email }) {
+    const user = await User.create({ user_id, user_pw, user_email });
+    return user;
   },
   async findOne(filter) {
-    FriendModel.findOne(filter).lean().exec();
+    return User.findOne(filter).exec();
   },
 };
