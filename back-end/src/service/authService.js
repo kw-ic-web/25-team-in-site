848f33d1d5536850db84dee3a0142913ddc8c506
leehyowon14 <57023119+leehyowon14@users.noreply.github.com>
2025-11-04 19:07:54 +0900

    typo: 오탈자 수정
    UserRepo -> UserRepository

--- back-end/src/service/authService.js
+++ back-end/src/service/authService.js
@@ -4,25 +4,23 @@ import { UserRepository } from "../repository/userRepository.js";
 export const AuthService = {
   async register(id, password, email) {
     const [byId, byEmail] = await Promise.all([
-      UserRepo.findOne({ id }),
-      UserRepo.findOne({ email }),
+      UserRepository.findOne({ id }),
+      UserRepository.findOne({ email }),
     ]);
     if (byId) throw new ConflictError("이미 등록된 아이디입니다.");
     if (byEmail) throw new ConflictError("이미 등록된 이메일입니다.");
 
     try {
-      const user = await UserRepo.create({
+      return await UserRepository.create({
         username,
         email,
         password,
       });
-      return user;
     } catch (err) {
       if (err?.code === 11000)
         throw new ConflictError("아이디/이메일이 이미 존재합니다.");
       throw err;
     }
-    //TODO: JWT 발급 및 리디렉션
   },
 
   async isIdAvailable(id) {
