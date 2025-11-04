734a6450c040d57e5134ed4ab7805656a32b639c
leehyowon14 <57023119+leehyowon14@users.noreply.github.com>
2025-11-04 20:23:21 +0900

    feat: 로그인 기능 구현

--- back-end/src/controllers/authController.js
+++ back-end/src/controllers/authController.js
@@ -2,12 +2,26 @@ export const AuthController = {
   async register(req, res, next) {
     try {
       const { id, email, password } = req.body;
-      const user = await req.services.user.register({
-        id,
-        email,
-        password,
-      });
+      const user = await req.services.auth.register(id, email, password);
       res.status(201).json(user);
+    } catch (e) {
+      next(e);
+    }
+  },
+
+  async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      const jwt = await req.services.auth.login(email, password);
+      res
+        .cookie("token", jwt, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "strict",
+          maxAge: 30 * 24 * 60 * 60 * 1000,
+        })
+        .status(200)
+        .json({});
     } catch (e) {
       next(e);
     }
--- back-end/src/service/authService.js
+++ back-end/src/service/authService.js
@@ -1,26 +1,45 @@
+import jwt from "jsonwebtoken";
+import config from "../config/env.js";
 import { ConflictError } from "../errors/conflict.js";
 import { UserRepository } from "../repository/userRepository.js";
+import { BadRequestError } from "../errors/badRequest.js";
+import { UnauthorizedError } from "../errors/unauthorized.js";
 
 export const AuthService = {
   async register(id, password, email) {
     const [byId, byEmail] = await Promise.all([
-      UserRepository.findOne({ id }),
-      UserRepository.findOne({ email }),
+      UserRepository.findOne({ user_id: id }),
+      UserRepository.findOne({ user_email: email }),
     ]);
     if (byId) throw new ConflictError("이미 등록된 아이디입니다.");
     if (byEmail) throw new ConflictError("이미 등록된 이메일입니다.");
 
     try {
-      return await UserRepository.create({
-        username,
-        email,
-        password,
+      const user = await UserRepository.create({
+        user_id: id,
+        user_email: email,
+        user_pw: password,
       });
+      return user;
     } catch (err) {
       if (err?.code === 11000)
         throw new ConflictError("아이디/이메일이 이미 존재합니다.");
       throw err;
     }
+  },
+  async login(email, password) {
+    if (!email || !password)
+      throw new BadRequestError("이메일/비밀번호를 입력하세요.");
+    const normEmail = email.trim().toLowerCase();
+    const user = await UserRepository.findOne({ user_email: normEmail });
+    if (!user)
+      throw new UnauthorizedError("이메일 또는 비밀번호가 올바르지 않습니다.");
+    const ok = await user.comparePassword(password);
+    if (!ok)
+      throw new UnauthorizedError("이메일 또는 비밀번호가 올바르지 않습니다.");
+    return jwt.sign({ user_id: user.user_id }, config.jwtSecret, {
+      noTimestamp: true,
+    });
   },
 
   async isIdAvailable(id) {
