cc4224d945efdb275c117c62e9673649a5b06c33
leehyowon14 <57023119+leehyowon14@users.noreply.github.com>
2025-11-10 17:57:10 +0900

    feat: create에서 보안 이슈 해결

--- back-end/src/repository/userRepository.js
+++ back-end/src/repository/userRepository.js
@@ -3,7 +3,7 @@ import { User } from "../entity/user.js";
 export const UserRepository = {
   async create({ user_id, user_pw, user_email }) {
     const user = await User.create({ user_id, user_pw, user_email });
-    return user;
+    return User.findById(user._id).select("-user_pw").exec();
   },
   async findOne(filter, { includePassword = false } = {}) {
     const query = User.findOne(filter);
