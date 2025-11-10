d24a0804dce46e7c7d557f38933634fa3dc56ab0
leehyowon14 <57023119+leehyowon14@users.noreply.github.com>
2025-11-10 17:51:32 +0900

    feat: 비밀번호 최대 길이 적용

--- back-end/src/dto/auth/registerRequest.js
+++ back-end/src/dto/auth/registerRequest.js
@@ -3,5 +3,8 @@ import { z } from "zod";
 export const RegisterRequestDto = z.object({
   user_id: z.string().min(3, "INVALID_REGISTER_REQUEST"),
   email: z.email("INVALID_EMAIL_FORMAT").trim().toLowerCase(),
-  password: z.string().trim().min(8, "INVALID_REGISTER_REQUEST"),
+  password: z
+    .string()
+    .min(8, "INVALID_REGISTER_REQUEST")
+    .max(128, "INVALID_REGISTER_REQUEST"),
 });
