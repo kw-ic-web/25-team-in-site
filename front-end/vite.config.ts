ec30b98fc7524a8fbdf4c81d9eb9d52e9c2af2da
taesungs <70329288+taesungs@users.noreply.github.com>
2025-11-13 09:43:42 +0900

    chore: vite 설정 변경

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,13 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// vite proxy + babel-react-compiler 함께 동작 버전
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://43.201.106.151:3000", // backend 루트까지만
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
+    },
+  },
+});
